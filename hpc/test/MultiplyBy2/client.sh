5807b174d103785967b1b64bcef29d505287ce3b
Elaphurus <105095085+LennoxLiu@users.noreply.github.com>
2024-01-23 10:29:13 +0100

    Add Makefile to MultiplyBy2

---
+++ hpc/test/MultiplyBy2/client.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+# export TEST_DELAY=1e4
+
+echo "Sending requests..." 
+ 
+for i in {1..100} 
+do 
+   # Expected output: {"output":[[200.0]]} 
+   # Check if curl output equals expected output 
+   # If not, print error message 
+ 
+   if [ "$(curl -s http://localhost:4242/Evaluate -X POST -d '{"name": "forward", "input": [[100.0]]}')" == '{"output":[[200.0]]}' ]; then 
+       echo -n "y" 
+   else 
+       echo -n "n" 
+       #echo "Error: curl output does not equal expected output" 
+   fi & 
+ 
+done 
+ 
+echo "Requests sent. Waiting for responses..." 
+ 
+wait
---
+++ hpc/test/MultiplyBy2/hq_scripts/job.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+#HQ --resource model=1
+#HQ --time-request=1m
+#HQ --time-limit=2m
+#HQ --stdout none
+#HQ --stderr none
+
+# Launch model server, send back server URL
+# and wait to ensure that HQ won't schedule any more jobs to this allocation.
+
+function get_avaliable_port {
+    # Define the range of ports to select from
+    MIN_PORT=1024
+    MAX_PORT=65535
+
+    # Generate a random port number
+    port=$(shuf -i $MIN_PORT-$MAX_PORT -n 1)
+
+    # Check if the port is in use
+    while lsof -Pi :$port -sTCP:LISTEN -t >/dev/null; do
+        # If the port is in use, generate a new random port number
+        port=$(shuf -i $MIN_PORT-$MAX_PORT -n 1)
+    done
+
+    echo $port
+}
+
+port=$(get_avaliable_port)
+export PORT=$port
+
+# Assume that server sets the port according to the environment variable 'PORT'.
+./server & 
+
+load_balancer_dir="./"
+
+
+host=$(hostname -I | awk '{print $1}')
+
+# Wait for model server to start
+while ! curl -s "http://$host:$port/Info" > /dev/null; do
+    sleep 1
+done
+
+# Write server URL to file identified by HQ job ID.
+mkdir -p "$load_balancer_dir/urls"
+echo "http://$host:$port" > "$load_balancer_dir/urls/url-$HQ_JOB_ID.txt"
+
+sleep infinity # keep the job occupied
