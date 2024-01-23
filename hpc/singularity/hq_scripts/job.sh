613cb7771c05c1042f0ce4dfe4c15240dedc7b17
Elaphurus <105095085+LennoxLiu@users.noreply.github.com>
2024-01-23 23:48:28 +0100

    Update job.sh

--- hpc/singularity/hq_scripts/job.sh
+++ hpc/singularity/hq_scripts/job.sh
@@ -36,21 +36,15 @@ mkdir -p ./output
 
 echo "Building singularity sandbox at ./tmpdir/$HQ_JOB_ID/ ."
 # make a copy of sandbox to avoid overwriting
-# $TMPDIR is the temporary directory at each node on Helix
 mkdir -p ./tmpdir/$HQ_JOB_ID/
 rm -rf ./tmpdir/$HQ_JOB_ID/*
 cp -r l2-sea.simg ./tmpdir/$HQ_JOB_ID/
-# need to pull the image from singularity hub first
-# singularity build --sandbox $TMPDIR/$HQ_JOB_ID/l2-sea.simg l2-sea.sif
-# singularity pull $TMPDIR/$HQ_JOB_ID/l2-sea.sif library://lennoxl/umbridge/l2-sea:latest
-# convert image from read-only to modifiable
-# singularity build --sandbox $TMPDIR/$HQ_JOB_ID/l2-sea.simg $TMPDIR/$HQ_JOB_ID/l2-sea.sif
 
 echo "Finish building singularity sandbox at ./tmpdir/$HQ_JOB_ID/ ."
 
 echo "Starting singularity server at http://$host:$port"
 # load umbridge server from local file
-singularity run --writable --bind ./load-balancer_singularity/umbridge-server:/umbridge-server --bind ./output:/output --pwd /umbridge-server ./tmpdir/$HQ_JOB_ID/l2-sea.simg $port &
+singularity run --writable --bind ./load-balancer_singularity/umbridge-server:/umbridge-server --bind ./tmpdir/$HQ_JOB_ID/output:/output --pwd /umbridge-server ./tmpdir/$HQ_JOB_ID/l2-sea.simg $port &
 
 load_balancer_dir="./"
 
