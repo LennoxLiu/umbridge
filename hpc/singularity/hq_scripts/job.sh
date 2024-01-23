aee3c5ae5d0bd4a8d5facb81969f0ae8eef98b61
Elaphurus <105095085+LennoxLiu@users.noreply.github.com>
2024-01-23 22:50:20 +0100

    Update job.sh

--- hpc/singularity/hq_scripts/job.sh
+++ hpc/singularity/hq_scripts/job.sh
@@ -37,7 +37,7 @@ mkdir -p ./output
 echo "Building singularity sandbox at $TMPDIR/$HQ_JOB_ID ."
 # make a copy of sandbox to avoid overwriting
 # $TMPDIR is the temporary directory at each node on Helix
-mkdir -p $TMPDIR/$HQ_JOB_ID
+mkdir -p $TMPDIR/$HQ_JOB_ID/
 cp -r l2-sea.simg $TMPDIR/$HQ_JOB_ID/
 # need to pull the image from singularity hub first
 # singularity build --sandbox $TMPDIR/l2-sea.simg l2-sea.sif
