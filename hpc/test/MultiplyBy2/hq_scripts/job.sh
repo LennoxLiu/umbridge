1b321a42c3b9f6d2d19d2bfcb4cab0f4dbd79729
Elaphurus <105095085+LennoxLiu@users.noreply.github.com>
2024-02-19 00:43:28 +0100

    Update job.sh

--- hpc/test/MultiplyBy2/hq_scripts/job.sh
+++ hpc/test/MultiplyBy2/hq_scripts/job.sh
@@ -9,14 +9,14 @@
 
 function get_avaliable_port {
     # Define the range of ports to select from
-    MIN_PORT=60000
+    MIN_PORT=49152
     MAX_PORT=65535
 
     # Generate a random port number
     port=$(shuf -i $MIN_PORT-$MAX_PORT -n 1)
 
     # Check if the port is in use
-    while lsof -Pi :$port -sTCP:LISTEN -t >/dev/null; do
+    while lsof -Pi :$port -t >/dev/null; do
         # If the port is in use, generate a new random port number
         port=$(shuf -i $MIN_PORT-$MAX_PORT -n 1)
     done
