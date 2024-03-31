8c50b3d42c52fd258402a32b472e9f0e09f65a98
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2024-04-01 00:55:16 +0200

    Update qmcpy-client-l2-sea.py

--- hpc/singularity/qmcpy-client-l2-sea.py
+++ hpc/singularity/qmcpy-client-l2-sea.py
@@ -44,7 +44,8 @@ dnb2 = qp.DigitalNetB2(d)
 gauss_sobol = qp.Uniform(dnb2, lower_bound=[0.25,-6.6], upper_bound=[0.41,-5.7])
 
 integrand = UMBridgeWrapper(gauss_sobol, l2sea_fixed_design_params, config, parallel=True)
+print("Got all integrand, starting integration...")
 
-qmc_sobol_algorithm = qp.CubQMCSobolG(integrand, abs_tol=1e-1, n_init = 8, n_max = 8) #n_init=n_max=256, changed for test
+qmc_sobol_algorithm = qp.CubQMCSobolG(integrand, abs_tol=1e-1, n_init = 256, n_max = 256) # n_init must be >= 2**8
 solution,data = qmc_sobol_algorithm.integrate()
 print(data)
