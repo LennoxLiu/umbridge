246ff52fff57ff02273ded1adef589ac8d723cdf
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2024-04-08 01:21:20 +0200

    Update allocation_queue.sh

--- hpc/singularity/hq_scripts/allocation_queue.sh
+++ hpc/singularity/hq_scripts/allocation_queue.sh
@@ -5,6 +5,7 @@ hq alloc add slurm --time-limit 10m \
                    --backlog 1 \
                    --workers-per-alloc 1 \
                    --max-worker-count 5 \
+                   --resource "model=range(1-1)" \
                    --cpus=1 \
                    -- -p "devel" --account=bw22J001 # Add account id, remove before publishing
                 
