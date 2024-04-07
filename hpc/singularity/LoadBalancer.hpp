600c10edd53f780da42ea3b95e8fd376703bec43
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2024-04-08 00:26:08 +0200

    Update path

--- hpc/singularity/LoadBalancer.hpp
+++ hpc/singularity/LoadBalancer.hpp
@@ -7,7 +7,7 @@
 #include <tuple>
 #include <memory>
 #include <filesystem>
-#include "../lib/umbridge.h"
+#include "../../lib/umbridge.h"
 #include <unistd.h>
 
 // run and get the result of command
