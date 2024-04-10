a9cb527ebfe4285538a9fe344bf857f4e202196e
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2024-04-10 20:14:39 +0200

    Update job.sh

--- hpc/hq_scripts/job.sh
+++ hpc/hq_scripts/job.sh
@@ -17,7 +17,7 @@ MAX_PORT=65535
 port=$(shuf -i $MIN_PORT-$MAX_PORT -n 1)
 # Check if the port is in use
 try_count=0
-while [ lsof -Pi :$port -sTCP:LISTEN -t ]
+while lsof -Pi :$port -sTCP:LISTEN -t
 do
     echo "Port $port is in use, trying another port"
     # If the port is in use, generate a new port number
