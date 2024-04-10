c392d766f22a3df93a8d0f6f9adbefebd376fec5
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2024-04-10 16:04:58 +0200

    Change way to test port

--- hpc/hq_scripts/job.sh
+++ hpc/hq_scripts/job.sh
@@ -17,11 +17,9 @@ function get_avaliable_port {
     # Generate a random port number
     port=$(shuf -i $MIN_PORT-$MAX_PORT -n 1)
 
-    # Check if the port is in use
-    while lsof -Pi :$port -sTCP:LISTEN -t >/dev/null; do
-        # If the port is in use, generate a new random port number
-        port=$(shuf -i $MIN_PORT-$MAX_PORT -n 1)
-    done
+    while nc -z localhost $port ; do
+		port=$(shuf -i $MIN_PORT-$MAX_PORT -n 1) 
+	done;
 
     echo $port
 }
