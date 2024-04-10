2b4c70552f35ebb224129cf837e1410f9d723fa4
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2024-04-10 17:51:18 +0200

    update Makefile

--- hpc/test/MultiplyBy2/minimal-server.cpp
+++ hpc/test/MultiplyBy2/minimal-server.cpp
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
     else
     {
         port = atoi(port_cstr);
-        std::cout << "Using port [" << port << " ] as specified by environment variable PORT." << std::endl;
+        std::cout << "Using port [ " << port_cstr << " ] as specified by environment variable PORT." << std::endl;
     }
 
     char const *delay_cstr = std::getenv("TEST_DELAY");
