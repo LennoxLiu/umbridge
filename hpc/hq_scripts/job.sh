03b5b0d3f7e09163b4c11cac39570a480de1685a
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2024-04-15 10:30:10 +0200

    Update job.sh

--- hpc/hq_scripts/job.sh
+++ hpc/hq_scripts/job.sh
@@ -49,7 +49,14 @@ host=$(hostname -I | awk '{print $1}')
 
 timeout=60 # timeout in seconds
 echo "Waiting for model server to respond at $host:$port..."
-if timeout $timeout curl -s "http://$host:$port/Info" > /dev/null; then
+
+function server_is_up() {
+    while ! curl -s "http://$host:$port/Info" > /dev/null; do
+        sleep 1
+    done
+}
+
+if timeout $timeout server_is_up; then
     echo "Model server responded within $timeout seconds"
 else
     echo "Timeout: Model server did not respond within $timeout seconds"
